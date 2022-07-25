c91aae59da26e2da532587df57ec40deec9ac135
Cactus Vladek <vlad.demianiuk.nemo2018@gmail.com>
2022-07-25 19:40:17 +0300

    Initial commit

---
+++ src/main/java/net/cactus/samuraimod/SamuraiMod.java
@@ -0,0 +1,15 @@
+package net.cactus.samuraimod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SamuraiMod implements ModInitializer {
+	public static final String MOD_ID = "samuraimod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/cactus/samuraimod/SamuraiModClient.java
@@ -0,0 +1,10 @@
+package net.cactus.samuraimod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SamuraiModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
